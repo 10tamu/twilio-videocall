dcc8011285b2d96fbfacba7784b2fa6114ebe589
palaktank <101913785@student.swin.edu.au>
2020-09-09 00:03:50 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
+import VideoComponent from "./components/videoComponent";
 import './App.css';
+
 
 function App() {
   return (
@@ -10,14 +12,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <VideoComponent/>
+       
       </header>
     </div>
   );
